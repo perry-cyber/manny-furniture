5db03d6ef24ed16f63844badb44b878bd2cd54fe
perry-cyber <victorsimon482@gmail.com>
2025-03-13 01:47:56 -0600

    dell2

--- src/app/catelog/page.tsx
+++ src/app/catelog/page.tsx
@@ -1,9 +1,17 @@
+import FloatingNavbar from '@/components/FloatingNav'
+import { WavyBackground } from '@/components/ui/wavy-background'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      catelog
+      <FloatingNavbar/>
+            <WavyBackground className="w-full mx-auto flex items-center justify-center pb-10">
+            <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+              OUR CATELOG
+            </p>
+            
+          </WavyBackground>
     </div>
   )
 }
